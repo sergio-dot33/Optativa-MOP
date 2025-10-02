1738ef1445675bb27d9ac35a396744bfcc09d266
sergio-dot33 <sergio.dot11@gmail.com>
2025-10-02 19:50:11 +0200

    update

--- Proyecto inicial/src/Main.java
+++ Proyecto inicial/src/Main.java
@@ -1,2 +1,21 @@
 public class Main {
+
+    //sergio el mejor
+    /*
+    eseee
+    ddr
+    maricon de mierda
+     */
+    //todo me queda pendiente 
+    public static void main(String[] arg){
+        System.out.println("Mi primer programa en Java");
+        System.out.println("Mi segunda linea del programa en Java");
+
+    }
+
+    public void nombreMetodo(){
+
+
+    }
+
 }
