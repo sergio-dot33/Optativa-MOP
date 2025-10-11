94ccfdd27e9bef21df96fd108dac8e7cd322c5bd
sergio-dot33 <sergio.dot11@gmail.com>
2025-10-11 12:21:08 +0200

    update

--- Proyecto inicial/src/Main.java
+++ Proyecto inicial/src/Main.java
@@ -4,14 +4,49 @@ public class Main {
     /*
     ejemplo 2 comentarios
      */
-    //todo me queda pendiente
+    //TODO me queda pendiente
     public static void main(String[] arg){
-        System.out.println("Mi primer programa en Java");
-        System.out.println("Mi segunda linea del programa en Java");
+        System.out.println("Primer programa en Java");
+        String miNombre = "Sergio";
+
+        Character letraCompleja = 'A';
+        char letraDNI = 'A';
+
+        Integer edadCompleja = 31;
+        int edad = 31;
+
+        Double alturaCompleja = 1.55;
+        double altura = 1.33;
+
+        Float pesoCompleja = 55.33f;
+        float peso = 69.55f;
+
+        Boolean experienciaCompleja = false;
+        boolean experiencia = true;
+
+
+
+
+        System.out.println("Mi nombres es: "+miNombre);
+        System.out.println("Mi edad es: "+edad);
+        System.out.println("La letra de mi DNI es: "+letraDNI+" gg");
+        System.out.println("Mi altura es: "+altura);
+        System.out.println("Mi peso de gordo es "+peso);
+        System.out.println("Mi experiencia es: "+experiencia);
+        System.out.println("El valor del numero PI es: "+Math.PI);
+        System.out.println("El valor maximo de un int es: "+Integer.MAX_VALUE);
+        System.out.println("El valor maximo de un int es: "+Integer.MIN_VALUE);
 
     }
+    // \n es salto de linea
+    //null
+    //por ejemplo edad mas simbolo + aumenta en uno el valor de la variable
+    //concatenar con el + "mi nombre es: "+miNombre+" apellido"
+    //Object puedes guardar cualquier cosa
+    //palabra reserva this para llamar a una clase fuera del metodo
 
-    public void nombreMetodo(){
+
+    public static void nombreMetodo(){
 
 
     }
---
+++ Proyecto inicial/src/MainMetodos.java
@@ -0,0 +1,17 @@
+public class MainMetodos {
+    public static void main(String[] args) {
+        System.out.println("Main para probar metodos y variables");
+
+
+
+
+    }
+
+    public static void saludar(String data){
+        System.out.println("Enhorabuena "+data+" has llamado a tu primer metodo");
+
+
+
+    }
+
+}
